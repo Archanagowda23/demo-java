3bb5f10d655ae0b61bdd26f4f48ff8b9d606fe8f
Archanagowda23 <archanar249@gmail.com>
2022-03-17 21:46:22 +0530

    initial commit

---
+++ src/com/bridgelabz/demojava/PatternProgram.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.demojava;
+
+public class PatternProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to pattern program");
+    }
+}
