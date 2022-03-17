986064ccafc52814de40e7298e5790f5d4aaacd5
Archanagowda23 <archanar249@gmail.com>
2022-03-17 21:52:35 +0530

    added square pattern

--- src/com/bridgelabz/demojava/PatternProgram.java
+++ src/com/bridgelabz/demojava/PatternProgram.java
@@ -1,8 +1,23 @@
 package com.bridgelabz.demojava;
 
+import java.util.Scanner;
+
 public class PatternProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to pattern program");
+    public void squarePattern() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter no");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print("*");
+            System.out.println();
+        }
     }
-}
+        public static void main (String[]args){
+            System.out.println("Welcome to pattern program");
+            PatternProgram object = new PatternProgram();
+            object.squarePattern();
+        }
+    }
